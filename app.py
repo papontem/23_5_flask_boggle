482cebae75469176df1e84bb32a2db044f251733
papontemjetix <papontem@gmail.com>
2023-06-29 15:43:56 -0400

    init for exercise 23.5, sorry forgot

---
+++ app.py
@@ -0,0 +1,10 @@
+from boggle import Boggle
+
+boggle_game = Boggle()
+
+"""
+Step#1 
+looking at app.py no flask code was given, we will insert our own. 
+this is where your routing logic should go.
+PAM: understood Will do!
+"""
